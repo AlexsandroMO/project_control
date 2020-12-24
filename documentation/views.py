71117a1d3e91308836863ecd4277fd674d89b860
AlexsandroMO <sandrobass@htomail.com>
2020-12-24 18:17:57 -0300

    update

--- documentation/views.py
+++ documentation/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import CotationForm
-#from django.contrib import messages
+from django.contrib import messages
 from .models import MyProject, PageT, DocT, DocumentStandard, Subject, Action, StatusDoc, Employee, Cotation, Upload, ProjectValue
 
 
